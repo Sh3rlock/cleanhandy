ea94fd1b7a0237030eff273a02f5b50aaf05c519
Sh3rlock <matyass91@gmail.com>
2025-10-24 16:45:32 +0300

    Postgres Database setup 3

--- quotes/context_processors.py
+++ quotes/context_processors.py
@@ -4,9 +4,14 @@ from .models import ServiceCategory
 from .models import ContactInfo, AboutContent
 
 def all_services(request):
-    return {
-        'all_services': Service.objects.all()
-    }
+    try:
+        return {
+            'all_services': Service.objects.all()
+        }
+    except Exception as e:
+        return {
+            'all_services': []
+        }
 
 def newsletter_form(request):
     return {
@@ -14,26 +19,45 @@ def newsletter_form(request):
     }
 
 def cleaning_service_category(request):
-    return {
-        'home_service_cat': ServiceCategory.objects.filter(name__iexact='home').first(),
-        'commercial_service_cat': ServiceCategory.objects.filter(name__iexact='commercial').first()
-    }
+    try:
+        return {
+            'home_service_cat': ServiceCategory.objects.filter(name__iexact='home').first(),
+            'commercial_service_cat': ServiceCategory.objects.filter(name__iexact='commercial').first()
+        }
+    except Exception as e:
+        return {
+            'home_service_cat': None,
+            'commercial_service_cat': None
+        }
 
 def handyman_service_category(request):
-    return {
-        'handyman_service_cat': ServiceCategory.objects.filter(name__iexact='handyman').first()
-    }
+    try:
+        return {
+            'handyman_service_cat': ServiceCategory.objects.filter(name__iexact='handyman').first()
+        }
+    except Exception as e:
+        return {
+            'handyman_service_cat': None
+        }
 
 def contact_info(request):
     """Add contact information to all templates"""
-    contact_info = ContactInfo.get_active()
+    try:
+        contact_info = ContactInfo.get_active()
+    except Exception as e:
+        # Handle case where table doesn't exist yet (during migrations)
+        contact_info = None
     return {
         'contact_info': contact_info
     }
 
 def about_content(request):
     """Add about content to all templates"""
-    about_content = AboutContent.get_active()
+    try:
+        about_content = AboutContent.get_active()
+    except Exception as e:
+        # Handle case where table doesn't exist yet (during migrations)
+        about_content = None
     return {
         'about_content': about_content
     }
